7698baa243df7fbed870f38d466eb7c09b8ecb8e
Ivan Grasso <grassoivan@gmail.com>
2021-05-25 02:10:38 -0300

    timestamp microservice

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // server.js
 // where your node app starts
+require('dotenv').config();
 
 // init project
 var express = require('express');
@@ -25,8 +26,52 @@ app.get("/api/hello", function (req, res) {
 });
 
 
-
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+/** Timestamp Microservice */
+
+app.get('/api', function (req, res) {
+  res.json({
+    "unix": Date.now(),
+    "utc": new Date().toUTCString()
+  })
+});
+
+app.get('/api/:date', function (req, res) {
+  if (isNaN(req.params.date)) {
+    var date = new Date(req.params.date)
+    if (date.toString() === "Invalid Date") {
+      res.json({ error: "Invalid Date" })
+    } else {
+      res.json({
+        "unix": Date.parse(req.params.date),
+        "utc": new Date(req.params.date).toUTCString()
+      })
+    }
+  } else {
+    var date = parseInt(req.params.date)
+    res.json({
+      "unix": date,
+      "utc": new Date(date).toUTCString()
+    })
+  }
+});
+
+/*
+- [x] A request to /api/:date? with a valid date should return a JSON object with a unix key that is a Unix timestamp of the input date in milliseconds
+
+- [x] A request to /api/:date? with a valid date should return a JSON object with a utc key that is a string of the input date in the format: Thu, 01 Jan 1970 00:00:00 GMT
+
+- [x] A request to /api/1451001600000 should return { unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" }
+
+- [x] Your project can handle dates that can be successfully parsed by new Date(date_string)
+
+- [x] If the input date string is invalid, the api returns an object having the structure { error : "Invalid Date" }
+
+- [x] An empty date parameter should return the current time in a JSON object with a unix key
+
+- [x] An empty date parameter should return the current time in a JSON object with a utc key
+ */
